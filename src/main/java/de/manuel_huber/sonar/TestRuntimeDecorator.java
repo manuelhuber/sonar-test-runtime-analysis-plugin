cd1c498a11bbfa7e9c7cc3ec87c253989535546d
Manuel Huber <manuel.huber@qaware.de>
2015-10-28 11:10:43 +0100

    Extracted functionallity to new methods

--- src/main/java/de/manuel_huber/sonar/TestRuntimeDecorator.java
+++ src/main/java/de/manuel_huber/sonar/TestRuntimeDecorator.java
@@ -1,6 +1,8 @@
 package de.manuel_huber.sonar;
 
+import com.google.common.collect.Iterables;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -10,8 +12,10 @@ import de.manuel_huber.sonar.model.TestFileModel;
 import org.sonar.api.batch.Decorator;
 import org.sonar.api.batch.DecoratorContext;
 import org.sonar.api.component.ResourcePerspectives;
+import org.sonar.api.config.Settings;
 import org.sonar.api.issue.Issuable;
 import org.sonar.api.issue.Issue;
+import org.sonar.api.measures.Measure;
 import org.sonar.api.resources.Project;
 import org.sonar.api.resources.Qualifiers;
 import org.sonar.api.resources.Resource;
@@ -26,28 +30,19 @@ import org.sonar.api.test.TestPlan;
 public class TestRuntimeDecorator implements Decorator {
 
     private final ResourcePerspectives resourcePerspectives;
-    String baseURL;
+    private final Settings settings;
+    private DecoratorContext context;
+    private Resource resource;
 
-
-    public TestRuntimeDecorator(ResourcePerspectives resourcePerspectives) {
+    public TestRuntimeDecorator(ResourcePerspectives resourcePerspectives, Settings settings) {
         this.resourcePerspectives = resourcePerspectives;
+        this.settings = settings;
     }
 
-
+    // Needed to run tests
     public TestRuntimeDecorator() {
         this.resourcePerspectives = null;
-    }
-
-
-    public void decorate(Resource resource, DecoratorContext context) {
-        if (resource.getQualifier().equals(Qualifiers.UNIT_TEST_FILE)) {
-            try {
-                checkTestCases(resource, context);
-            } catch (Exception e) {
-                //good luck
-                System.out.println(e.toString());
-            }
-        }
+        this.settings = null;
     }
 
 
@@ -56,22 +51,29 @@ public class TestRuntimeDecorator implements Decorator {
     }
 
 
-    public TestFileModel getTestFileModel(Resource resource) throws UnirestException {
-        baseURL = TestRuntimePlugin.getSonarBaseURL();
-        HttpResponse<JsonNode> getResponse = Unirest.get(baseURL + "/api/tests/show")
-                .field("key", resource.getEffectiveKey()).asJson();
-        Gson gson = new Gson();
-        JsonNode body = getResponse.getBody();
-        return gson.fromJson(String.valueOf(body), TestFileModel.class);
+    public void decorate(Resource resource, DecoratorContext context) {
+        if (resource.getQualifier().equals(Qualifiers.UNIT_TEST_FILE)) {
+            System.out.println("----------------------Decorator----------------------");
+            this.context = context;
+            this.resource = resource;
+            try {
+                TestCaseModel[] previousTestCasesArray = getTestFileModel().getTests();
+                TestPlan currentTestPlan = resourcePerspectives.as(MutableTestPlan.class, resource);
+                checkTestCases(previousTestCasesArray, currentTestPlan);
+            } catch (UnirestException e) {
+                System.out.println("There was a problem getting the data from the Rest call");
+                System.out.println(e.toString());
+            } finally {
+                System.out.println("----------------------Decorator----------------------");
+            }
+        }
+
+        System.out.println("TEST");
     }
 
 
-    public void checkTestCases(Resource resource, DecoratorContext context) throws UnirestException {
-        System.out.println("----------------------Decorator----------------------");
-
-        TestCaseModel[] previousTestCasesArray = getTestFileModel(resource).getTests();
-        TestPlan currentTestPlan = resourcePerspectives.as(MutableTestPlan.class, resource);
-
+    public void checkTestCases(TestCaseModel[] previousTestCasesArray, TestPlan currentTestPlan) {
+        saveFirstTime(currentTestPlan);
         for (TestCaseModel previousTestCase : previousTestCasesArray) {
             Iterable<TestCase> currentTestCaseIterable = currentTestPlan.testCasesByName(previousTestCase.getName());
             //this Iterable should only contain 1 item
@@ -92,7 +94,23 @@ public class TestRuntimeDecorator implements Decorator {
                 }
             }
         }
-        System.out.println("----------------------Decorator----------------------");
+
+    }
+
+
+    /**
+     * Makes a REST call to get the Data from the previous run
+     *
+     * @return
+     * @throws UnirestException
+     */
+    public TestFileModel getTestFileModel() throws UnirestException {
+        String baseURL = settings.getString("sonar.core.serverBaseURL");
+        HttpResponse<JsonNode> getResponse = Unirest.get(baseURL + "/api/tests/show")
+                .field("key", resource.getEffectiveKey()).asJson();
+        Gson gson = new Gson();
+        JsonNode body = getResponse.getBody();
+        return gson.fromJson(String.valueOf(body), TestFileModel.class);
     }
 
 
@@ -110,4 +128,26 @@ public class TestRuntimeDecorator implements Decorator {
         }
         return percentileIncrease;
     }
+
+    public void saveFirstTime(TestPlan currentTestPlan) {
+        TestFileModel testFileModel = testPlanToTestFileModel(currentTestPlan);
+        Gson gson = new Gson();
+        String save = gson.toJson(testFileModel);
+        context.saveMeasure(new Measure(TestRuntimeMetric.TEST_RUNTIME_SAVE, save));
+    }
+
+    public TestFileModel testPlanToTestFileModel(TestPlan testPlan) {
+        Iterable<TestCase> testCases = testPlan.testCases();
+        testPlan.testCases();
+        TestCaseModel[] testCaseArray = new TestCaseModel[Iterables.size(testCases)];
+        int i = 0;
+        for (TestCase testCase : testCases) {
+            TestCaseModel model = new TestCaseModel();
+            model.setName(testCase.name());
+            model.setDurationInMs(testCase.durationInMs());
+            testCaseArray[i++] = model;
+        }
+        return new TestFileModel(testCaseArray);
+    }
+
 }
